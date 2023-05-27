b4f27b1c8400c7f83acbaf515a2f1d34a3228ffc
hxchen <helloworld.vip@gmail.com>
2023-05-27 14:36:19 +0800

    add 世界史

--- xique/src/main/java/com/a3fun/game/xique/controller/HomeController.java
+++ xique/src/main/java/com/a3fun/game/xique/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.a3fun.game.xique.controller;
 
+import org.springframework.boot.configurationprocessor.json.JSONException;
+import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,4 +14,13 @@ public class HomeController {
     public String Index(){
         return "Welcome to come here!";
     }
+
+    @ResponseBody
+    @GetMapping(value = "/books")
+    public String books() throws JSONException {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("世界史", "100");
+        return jsonObject.toString();
+    }
+
 }
